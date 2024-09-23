9d240ec3300b767a50230043aca968978f4ce586
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-09-23 17:49:28 +0530

    added

--- python01.py
+++ python01.py
@@ -1,15 +1,23 @@
 todos =[]
 
 while True:
-    user_actions = input("Type ,add ,show and exist:  ")
+    user_actions = input("Type ,add ,edit ,show and exit:  ")
+    user_actions = user_actions.strip()
 
     match user_actions:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
-            print(todos)
-        case 'exist':
+        case 'show' |  'display': #  (|) bitwise and or
+            for items in todos:
+                items= items.title()
+                print(items)
+        case 'edit':
+            number = int(input("enter the number of todo edit: "))
+            number = number-1
+            existing_todo = todos[number]
+            print(existing_todo)
+        case 'exit':
             break
 print("Bye!")
 
