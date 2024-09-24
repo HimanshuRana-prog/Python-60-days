323d390d01e1612dd0412a5da007ab7c1710498e
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-09-24 18:13:12 +0530

    001 file added

---
+++ for-loop.py
@@ -0,0 +1,5 @@
+food = ['apple:1','mango:2','banana:3','graphes:4']
+
+for items in food:
+    print(items)
+   
--- python01.py
+++ python01.py
@@ -1,16 +1,26 @@
 todos =[]
 
 while True:
-    user_actions = input("Type ,add ,show and exist:  ")
+    user_actions = input("Type ,add ,edit ,show and exit:  ")
+    user_actions = user_actions.strip()
 
     match user_actions:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
-            for items in todos:
-                print(items)
-        case 'exist':
+        case 'show' |  'display': #  (|) bitwise and or
+            for index,items in enumerate(todos):
+               print(f"{index+1}:){items}")
+        case 'edit':
+            number = int(input("enter the number of todo edit: "))
+            number = number-1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        case 'complete':
+            number = int(input("enter the number of todo complete: "))
+            todos.pop(number-1)
+            
+        case 'exit':
             break
 print("Bye!")
 
