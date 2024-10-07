ee7fd5fb9a6a9485ba580160487e3e2f2d9b78de
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-07 18:29:53 +0530

    Update the gui file

--- gui.py
+++ gui.py
@@ -1,5 +1,10 @@
 import function01
 import FreeSimpleGUI as sg
+import time
+
+sg.theme("Neon Blue1")
+
+clock = sg.Text('',key='clock')
 
 label = sg.Text("Type in To-do")
 input_box = sg.InputText(tooltip= "Enter Todo",key="Todo")
@@ -7,17 +12,23 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values= function01.get_todos(),
                       key = 'todos',enable_events= True,size= [45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
-layout = [[label],[input_box,add_button],[list_box,edit_button]]
+layout = [[label],
+          [input_box,add_button],
+          [list_box,edit_button,complete_button],
+          [exit_button]]
 
 window = sg.Window("My To-Do App", #window class is typically used to create a new window for your application
-                   layout= layout, 
-                   font=('Helvetica',15)) 
+                   layout= [[clock],[label],
+                   [input_box,add_button],
+                   [list_box,edit_button,complete_button],
+                   [exit_button]], font=('Helvetica',15),button_color='Black') 
 
 while True:
-   event, values= window.read()
-   print(event)
-   print(values)
+   event, values= window.read(timeout=200)
+   window['clock'].update(value = time.strftime("%b %d, %Y %H:%M:%S"))
    
    match event:
     case "Add":
@@ -28,14 +39,33 @@ while True:
         window['todos'].update(values = todos)
 
     case "Edit":
-        todo_to_edit = values['todos'][0]
-        new_todo = values['Todo']
+        try:
+            todo_to_edit = values['todos'][0]
+            new_todo = values['Todo']
 
-        todos = function01.get_todos()
-        index = todos.index(todo_to_edit)
-        todos[index] = new_todo
-        function01.write_todos(todos)
-        window['todos'].update(values = todos)
+            todos = function01.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            function01.write_todos(todos)
+            window['todos'].update(values = todos)
+        except IndexError:
+            sg.popup("Please Select an item First: ",font=('Helvetica',20)) #opup is a type of window that appears over your main application window to display error message
+    
+    case 'Complete':
+        try:
+            todo_to_complete = values['todos'][0]
+            todos = function01.get_todos()
+            todos.remove(todo_to_complete)
+
+            function01.write_todos(todos)
+            window['todos'].update(values = todos)
+        except IndexError:
+            sg.popup("Please select the Item:", font=('Helvetica',20))
+                     
+                     
+    case "Exit":
+           break
+
 
     case 'todos':
         window['Todo'].update(value=values['todos'][0])
