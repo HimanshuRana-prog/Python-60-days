24fd9f011b9b8bff152239158c009c45a1353974
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-07 18:30:15 +0530

    files added

--- text-folder/zip_creator.py
+++ text-folder/zip_creator.py
@@ -5,7 +5,7 @@ def make_archive(filepaths,dest_dir):
     dest_path = pathlib.Path(dest_dir,"compressed.zip")
     with zipfile.ZipFile(dest_path,'w') as archive:
         for filepath in filepaths:
-            archive.write(filepath)
+            archive.write(filepath,arcname= filepath.name)
         
 if __name__ == "__name__":
     make_archive(filepaths= ["ex-01.py","ex-02.py"],dest_dir= "dest")
