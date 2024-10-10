330eebe30e7f67531bf02ce5b9268e935746a68d
Himanshu Rana <himanshu.rana@compunneldigital.com>
2024-10-10 11:05:42 +0530

    added

--- Bonus-Examples/ex-12.py
+++ Bonus-Examples/ex-12.py
@@ -1,14 +1,15 @@
 import streamlit as st
 from PIL import Image
-# start the  camera
-camera_image  = st.camera_input("Camera")
 
-# Create a pillow image instance
-img = Image.open(camera_image)
+# Starts the camera
+camera_image = st.camera_input("Camera")
 
-# Convert the pillow image to grayscale
-gray_img = img.convert("L")
+if camera_image:
+    # Create a pillow image instance 
+    img = Image.open(camera_image)
 
-# Render the grayscale image on the webpage
-st.image(gray_img)
- 
+    # covert the pillow image to grayscale
+    gray_img = img.convert("L")
+    
+    # Render the grayscale image on the webpage
+    st.image(gray_img)
